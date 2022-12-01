db0513877ea4c57c2fd5c0a7b7df8d481000dfb4
Coldsky22 <bingalik@mail.ru>
2022-12-01 13:43:03 +0300

    rev01

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 handler404 = 'core.views.page_not_found'
+handler403 = 'core.views.permission_denied'
+handler500 = 'core.views.server_error'
 
 urlpatterns = [
     path('', include('posts.urls', namespace="posts")),
